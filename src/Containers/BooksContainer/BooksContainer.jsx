c46aed7890a54215b1fe9eff307e24caa17a1669
Maduri Vassaramo <maduri@vinterior.co>
2022-03-12 14:03:42 +0100

    add fetch request to get books

--- src/Containers/BooksContainer/BooksContainer.jsx
+++ src/Containers/BooksContainer/BooksContainer.jsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const BooksContainer = () =>  {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
+
+  async function fetchBooks () {
+    try {
+      const title = 'why we sleep'
+      const result = await fetch(`http://openlibrary.org/search.json?title=${title}`)
+      const data = await result.json()
+      const books = await data.docs
+      setBooks(books)
+    } catch (e) {
+      return e
+    }
+  }
+
   return (
     <h1>Books!</h1>
   )
