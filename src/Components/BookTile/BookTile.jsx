7395e735a081e6869362cda82c63b8e8bbf3e6a8
Maduri Vassaramo <maduri@vinterior.co>
2022-03-12 16:12:16 +0100

    display book cover or default image if unavailable + corresponding tests

--- src/Components/BookTile/BookTile.jsx
+++ src/Components/BookTile/BookTile.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 
 const BookTile = props => {
-  console.log({ props })
+  console.log(props)
+  
+  function renderBookCoverImage () {
+    console.log('COVER', props.book.cover_i)
+    return (
+      <img
+        alt={`${props.book.title} cover`}
+        src={`https://covers.openlibrary.org/b/id/${props.book.cover_i}-M.jpg`}
+      />
+    )
+  }
+
+  function renderDefaultCoverImage () {
+    return (
+      <img
+        alt='book cover unavailable'
+        src='https://dhmckee.com/wp-content/uploads/2018/11/defbookcover-min.jpg'
+      />
+    )
+  }
+
   return (
-    <><h3>{props.book.title}</h3></>
+    <>
+      <h3>{props.book.title}</h3>
+      <h5 aria-label='author'>Author: {props.book.author_name}</h5>
+      {props.book.cover_i
+        ? renderBookCoverImage()
+        : renderDefaultCoverImage()
+      }
+    </>
   )
 }
 
--- src/Components/BookTile/BookTile.test.js
+++ src/Components/BookTile/BookTile.test.js
@@ -4,10 +4,31 @@ import '@testing-library/jest-dom'
 import BookTile from './BookTile'
 
 describe('BookTile', () => {
-  it('renders the component', () => {
+  it('displays the book title', () => {
     const book = { title: 'random book title' }
     render(<BookTile book={book} />)
 
     expect(screen.getByRole('heading', { name: 'random book title' })).toBeInTheDocument()
+  })
+
+  it('displays the book author', () => {
+    const book = { title: 'random book title', author_name: 'author name' }
+    render(<BookTile book={book} />)
+    screen.debug()
+    expect(screen.getByRole('heading', { name: 'author' })).toBeInTheDocument()
+  })
+
+  it('displays the book cover image if there is an isbn', () => {
+    const book = { title: 'book with image', author_name: 'author name', cover_i: 12345 }
+    render(<BookTile book={book} />)
+
+    expect(screen.getByRole('img', { name: 'book with image cover'})).toBeVisible()
+  })
+
+  it('displays a default image if book has no isbn', () => {
+    const book = { title: 'book with image', author_name: 'author name' }
+    render(<BookTile book={book} />)
+
+    expect(screen.getByRole('img', { name: 'book cover unavailable'})).toBeVisible()
   })
 })
