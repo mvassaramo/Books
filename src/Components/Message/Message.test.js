b1c40f7dede97591a8192d0e43037e367f20bec1
Maduri Vassaramo <maduri@vinterior.co>
2022-03-14 09:47:17 +0100

    update tests

--- src/Components/Message/Message.test.js
+++ src/Components/Message/Message.test.js
@@ -2,16 +2,10 @@ import { render, screen } from '@testing-library/react'
 import Message from './Message'
 
 describe('Message', () => {
-  it('renders the component', () => {
-    render(<Message />)
-
-    expect(screen.getByRole('heading')).toBeInTheDocument()
-  })
-
   it('displays the given props', () => {
     const children='random message to be shown'
     render(<Message children={children} />)
 
-    expect(screen.getByText('random message to be shown')).toBeVisible()
+    expect(screen.getByRole('heading', { name: 'random message to be shown' })).toBeVisible()
   })
 })
